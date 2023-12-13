79f223748f88387c00197f8026f0b5722bb35d60
PatraShuvankar <153713337+PatraShuvankar@users.noreply.github.com>
2023-12-13 18:50:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const photos = document.querySelectorAll(".photos img");
-  let currentPhotoIndex = 0;
+document.addEventListener('DOMContentLoaded', function () {
+  const images = document.querySelectorAll('.photos img');
+  let currentIndex = 0;
 
-  function showNextPhoto() {
-    photos[currentPhotoIndex].style.opacity = 0;
-    currentPhotoIndex = (currentPhotoIndex + 1) % photos.length;
-    photos[currentPhotoIndex].style.opacity = 1;
+  function showNextImage() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].classList.add('active');
   }
 
-  // Start the slideshow
-  setInterval(showNextPhoto, 5000); // Change the time (in milliseconds) to control the speed of the slideshow
+  images[currentIndex].classList.add('active');
+  setInterval(showNextImage, 3000); // Change the interval as needed
 });
