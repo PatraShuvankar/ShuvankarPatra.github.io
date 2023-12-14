1315ffb3747683955ab2368d6fa3d01edc3fb256
PatraShuvankar <153713337+PatraShuvankar@users.noreply.github.com>
2023-12-14 11:17:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const images = document.querySelectorAll('.photos img');
-  let currentIndex = 0;
-
-  function showNextImage() {
-    images[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % images.length;
-    images[currentIndex].classList.add('active');
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to open the resume modal
+  function openResumeModal() {
+    document.getElementById("resumeModal").style.display = "block";
   }
 
-  images[currentIndex].classList.add('active');
-  setInterval(showNextImage, 5000); // Updated interval to 5 seconds
+  // Function to close the resume modal
+  function closeModal() {
+    document.getElementById("resumeModal").style.display = "none";
+  }
+
+  // Close the modal if the user clicks outside the modal content
+  window.onclick = function (event) {
+    if (event.target === document.getElementById("resumeModal")) {
+      closeModal();
+    }
+  };
+
+  // Attach event listener to the resume button to open the modal
+  var resumeButton = document.getElementById("resumeButton");
+  if (resumeButton) {
+    resumeButton.addEventListener("click", openResumeModal);
+  }
+
+  // Attach event listener to the close button inside the modal
+  var closeButton = document.getElementById("closeButton");
+  if (closeButton) {
+    closeButton.addEventListener("click", closeModal);
+  }
 });
